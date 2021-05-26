0410874e4e4464a91fa91afd799d48a4d83bc91a
zrcoy <zhangruichaotyler@gmail.com>
2021-05-26 13:27:16 -0400

    building the most basic custom counting observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { interval, Subscription, Observable } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  constructor() {}
 
-  constructor() { }
+  firstObsSubscription: Subscription;
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe((num) => {
+    //   console.log(num);
+    // });
+
+    const myFirstCustomInvervalObs = Observable.create((observer) => {
+      let count: number = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = myFirstCustomInvervalObs.subscribe((count) => {
+      console.log(count);
+    });
   }
 
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
+  }
 }
